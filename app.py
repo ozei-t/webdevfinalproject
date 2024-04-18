a4e8ae2a41e0ebbb62fd0c27f282dc5306f37a19
ozei-t <cooldroid4@gmail.com>
2024-04-18 18:22:22 -0400

    gets songs but not page veiw is not how sight considers popularity?

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import lyricsgenius
-genius = lyricsgenius.Genius(WgL88zDw74vN0BHApKBu4Mfoz_EObXEFHKgxUlfdIhUcgZFvp5Vi7RcL0hs8J3rL)
+from lyricsgenius import Genius
 
-genius = Genius(WgL88zDw74vN0BHApKBu4Mfoz_EObXEFHKgxUlfdIhUcgZFvp5Vi7RcL0hs8J3rL)
+genius = Genius('WgL88zDw74vN0BHApKBu4Mfoz_EObXEFHKgxUlfdIhUcgZFvp5Vi7RcL0hs8J3rL')
 # lyric genius is installed on laptop instal on home pc pip install git+https://github.com/johnwmillr/LyricsGenius.git
 
+#
 def getArtistSongsByPop(artist_name, max_songs = None):
     artist = genius.search_artist(artist_name, max_songs=max_songs)
     if artist:
@@ -17,4 +17,11 @@ def getArtistSongsByPop(artist_name, max_songs = None):
         #sort the songs
         sorted_songs = sorted(songs,key=lambda x: x['stats']['pageviews'], reverse=True)
         return sorted_songs
+    
+def printSortedSongs(artist_name, max_songs=None):
+    sorted_songs = getArtistSongsByPop(artist_name,max_songs)
+    if sorted_songs:
+        for song in sorted_songs:
+            print(song['title'])
 
+printSortedSongs('Dominic Fike')
