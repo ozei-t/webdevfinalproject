4bfba6bb8849d9f92db96b1088a93ce3dbc1b68f
ozei-t <cooldroid4@gmail.com>
2024-04-18 17:52:10 -0400

    reminder to install LyricsGenius on home pc

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import lyricsgenius
 genius = lyricsgenius.Genius(WgL88zDw74vN0BHApKBu4Mfoz_EObXEFHKgxUlfdIhUcgZFvp5Vi7RcL0hs8J3rL)
 
 genius = Genius(WgL88zDw74vN0BHApKBu4Mfoz_EObXEFHKgxUlfdIhUcgZFvp5Vi7RcL0hs8J3rL)
+# lyric genius is installed on laptop instal on home pc pip install git+https://github.com/johnwmillr/LyricsGenius.git
